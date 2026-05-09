8d96cbec2db15d2e3eec5e409f2cd06807674c6e
ksk-wtnb <keisuke.watanabe09@gmail.com>
2026-05-09 14:43:37 +0900

    コードハイライトを追加

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,6 +1,21 @@
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+
+const CodeBlock = ({ node: _node, inline, className, children, ...props }) => {
+  const match = /language-(\w+)/.exec(className || '')
+  return !inline && match ? (
+    <SyntaxHighlighter language={match[1]} style={oneDark} {...props}>
+      {String(children).replace(/\n$/, '')}
+    </SyntaxHighlighter>
+  ) : (
+    <code className={className} {...props}>
+      {children}
+    </code>
+  )
+}
 
 function PostPage() {
   const { id } = useParams()
@@ -14,7 +29,7 @@ function PostPage() {
 
   return (
     <article>
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown components={{ code: CodeBlock }}>{content}</ReactMarkdown>
     </article>
   )
 }
